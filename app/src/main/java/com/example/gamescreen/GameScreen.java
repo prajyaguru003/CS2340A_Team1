01eadcae48aa9e5bcb9331058ac5aeb9f45ddb7a
rishigaddamanugu <rgaddamanugu3@gmail.com>
2023-10-10 19:14:29 -0400

    Added tile1.xml and fixed a couple of issues with GameScreen. Still have no clue why the try catch is working when there is a null pointer exception:

--- app/src/main/java/com/example/gamescreen/GameScreen.java
+++ app/src/main/java/com/example/gamescreen/GameScreen.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.pm.ActivityInfo;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 //import android.widget.ProgressBar;
@@ -13,20 +15,27 @@ public class GameScreen extends AppCompatActivity {
 
     //    private static Drawable character;
     //    private static int[] position;
-
+    private static final String TAG = "GameScreen";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.tile1);
+//        setContentView(R.layout.game_screen);
         //ProgressBar health = (ProgressBar) findViewById(R.id.health_bar);
         //Button interact = (Button) findViewById(R.id.interact);
         //Button up = (Button) findViewById(R.id.move_up);
         //Button down = (Button) findViewById(R.id.move_down);
         //Button left = (Button) findViewById(R.id.move_left);
         //Button right = (Button) findViewById(R.id.move_right);
-        ImageView player = (ImageView) findViewById(R.id.character);
-        player.setImageDrawable(ConfigScreen.getSprite());
+        ImageView player = (ImageView) findViewById(R.id.main_character);
+//        ImageView player = (ImageView) findViewById(R.id.character);
+        try {
+            player.setImageDrawable(ConfigScreen.getSprite());
+        } catch(NullPointerException e){
+            Log.d(TAG, "WE GOT A NULL EXCEPTION BOYS!");
+        }
         //position = new int[2];
-        showSelected();
+//        showSelected();
 
     }
 
