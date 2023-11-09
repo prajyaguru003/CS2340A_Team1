8f2706d6682a529952cbe5345bed40aa056e04b5
aaravshah026 <aaravshah026@gmail.com>
2023-11-09 14:50:53 -0500

    mistake

--- app/src/test/java/com/example/gamescreen/ExampleUnitTest.java
+++ app/src/test/java/com/example/gamescreen/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void maxScore500() {assertTrue(Integer.parseInt(GameScreen.getScore()) <= 500);}
+    @Test
+    public void minScore0() {assertTrue(Integer.parseInt(GameScreen.getScore()) >= 0);}
+
 }
