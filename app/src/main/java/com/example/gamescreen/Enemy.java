de8df7c3f4c04ba07c9d552c1e15081bd386368b
aaravshah026 <aaravshah026@gmail.com>
2023-11-10 22:35:32 -0500

    Create Enemy.java abstract class, transfer variables, add methods to EnemyFactory.java

---
+++ app/src/main/java/com/example/gamescreen/Enemy.java
@@ -0,0 +1,8 @@
+package com.example.gamescreen;
+
+public abstract class Enemy {
+    int hp;
+    int movementSpeed;
+    String name;
+
+}
--- app/src/main/java/com/example/gamescreen/EnemyFactory.java
+++ app/src/main/java/com/example/gamescreen/EnemyFactory.java
@@ -1,6 +1,9 @@
 package com.example.gamescreen;
 public abstract class EnemyFactory {
-    int hp;
-    int movementSpeed;
-    String name;
+
+    public abstract Enemy createEnemy1(int hp, int movementSpeed);
+    public abstract Enemy createEnemy2(int hp, int movementSpeed);
+    public abstract Enemy createEnemy3(int hp, int movementSpeed);
+    public abstract Enemy createEnemy4(int hp, int movementSpeed);
+    //method names to be decided later based on real enemy names!
 }
