1aa2ac0e29d8112b50361c45e6ea77b503178137
aaravshah026 <aaravshah026@gmail.com>
2023-11-12 18:33:57 -0500

    Add ImageView to display character in Enemy.java

--- app/src/main/java/com/example/gamescreen/Enemy.java
+++ app/src/main/java/com/example/gamescreen/Enemy.java
@@ -1,8 +1,11 @@
 package com.example.gamescreen;
+import android.widget.ImageView;
 
 public abstract class Enemy {
     int hp;
     int movementSpeed;
     String name;
+
+    ImageView character;
 
 }
