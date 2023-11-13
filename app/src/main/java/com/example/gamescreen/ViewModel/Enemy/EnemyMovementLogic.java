e9688a473a61faf0b48346bd4fd07e5cccbd83f7
rishigaddamanugu <rgaddamanugu3@gmail.com>
2023-11-12 21:29:53 -0500

    hello

---
+++ app/src/main/java/com/example/gamescreen/ViewModel/Enemy/EnemyMovementLogic.java
@@ -0,0 +1,8 @@
+package com.example.gamescreen.ViewModel.Enemy;
+
+public class EnemyMovementLogic {
+    Enemy1 enemy1 = new Enemy1();
+    Enemy2 enemy2 = new Enemy2();
+    Enemy3 enemy3 = new Enemy3();
+    Enemy4 enemy4 = new Enemy4();
+}
