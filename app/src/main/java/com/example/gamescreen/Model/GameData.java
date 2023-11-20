ebe0a3d4a29fcbdce8eb29961d85153d934117a8
“KruthikRavikanti” <ravikantikruthik@gmail.com>
2023-11-20 14:49:59 -0500

    Added more methods to gamedata

--- app/src/main/java/com/example/gamescreen/Model/GameData.java
+++ app/src/main/java/com/example/gamescreen/Model/GameData.java
@@ -1,8 +1,20 @@
 package com.example.gamescreen.Model;
-
+import java.uti.Scanner;
 public class GameData {
+
+    public
     public GameData data1;
     public GameData data2;
     int number = -1;
     double dataStorage = 2;
+
+    public int dealWithIt (int number1) {
+        number = number1;
+    }
+
+    public int dontDealWithIt (int number2) {
+        dataStorage = number2;
+    }
+
+    
 }
