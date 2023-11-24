d9e31eec7f78718e3c245c7aa9e24b1c61b9ad7f
aaravshah026 <aaravshah026@gmail.com>
2023-11-24 18:31:25 -0500

    Created base decorator class

---
+++ app/src/main/java/com/example/gamescreen/ViewModel/Powerup/HPBoost.java
@@ -0,0 +1,4 @@
+package com.example.gamescreen.ViewModel.Powerup;
+
+public class HPBoost {
+}
---
+++ app/src/main/java/com/example/gamescreen/ViewModel/Powerup/PowerupDecorator.java
@@ -0,0 +1,9 @@
+package com.example.gamescreen.ViewModel.Powerup;
+import com.example.gamescreen.ViewModel.Player;
+
+public class PowerupDecorator{
+    protected Player player;
+    public PowerupDecorator(Player player) {
+        this.player = player;
+    }
+}
