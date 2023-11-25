7575e939a1750d9bbe9a146ca29057841b31c7c1
aaravshah026 <aaravshah026@gmail.com>
2023-11-24 20:38:38 -0500

    Create initial code for HPBoost Powerup

--- app/src/main/java/com/example/gamescreen/ViewModel/Powerup/HPBoost.java
+++ app/src/main/java/com/example/gamescreen/ViewModel/Powerup/HPBoost.java
@@ -1,4 +1,10 @@
 package com.example.gamescreen.ViewModel.Powerup;
+import com.example.gamescreen.ViewModel.Player;
 
-public class HPBoost {
+public class HPBoost extends PowerupDecorator {
+    protected Player player;
+    public HPBoost(Player player) {
+        super(player);
+        // set HP boost x1.5
+    }
 }
