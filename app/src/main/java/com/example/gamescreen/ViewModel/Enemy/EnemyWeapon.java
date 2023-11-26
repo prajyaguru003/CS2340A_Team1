f70e76b85feb4b4a511380426bec628da38693e4
cjsimms02 <cjsimmons1111@gmail.com>
2023-11-26 18:34:30 -0500

    Add getter/setter for inUse variable to EnemyWeapon

--- app/src/main/java/com/example/gamescreen/ViewModel/Enemy/EnemyWeapon.java
+++ app/src/main/java/com/example/gamescreen/ViewModel/Enemy/EnemyWeapon.java
@@ -2,7 +2,8 @@ package com.example.gamescreen.ViewModel.Enemy;
 
 public class EnemyWeapon {
     private int damage;
-    public EnemyWeapon(int damage) {
+    private boolean inUse;
+    public EnemyWeapon(int damage, boolean inUse) {
         this.damage = damage;
     }
 
@@ -10,7 +11,15 @@ public class EnemyWeapon {
         return damage;
     }
 
+    public boolean isInUse() {
+        return inUse;
+    }
+    
     public void setDamage(int damage) {
         this.damage = damage;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
     }
 }
