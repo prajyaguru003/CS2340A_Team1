8d29caa000b50b7cd6c81f2c3918bd9af6eabd37
cjsimms02 <cjsimmons1111@gmail.com>
2023-11-26 19:53:07 -0500

    Added another weapon named Axe

---
+++ app/src/main/java/com/example/gamescreen/ViewModel/Enemy/Axe.java
@@ -0,0 +1,32 @@
+package com.example.gamescreen.ViewModel.Enemy;
+
+public class Axe extends Weapon {
+    private int damage;
+    private int durability;
+    private boolean inUse;
+    private int critCount;
+    private int critical;
+    private Bullet[] ammo;
+    public Axe(int damage, int durability, boolean inUse, int critical) {
+        this.damage = damage;
+        this.durability = durability;
+        this.inUse = inUse;
+        this.critical = critical;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
+    }
+}
