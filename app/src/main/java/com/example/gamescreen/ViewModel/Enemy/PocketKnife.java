bc039461743e322580916ba37a3c96a3ecf34c13
rishigaddamanugu <rgaddamanugu3@gmail.com>
2023-11-26 23:38:59 -0500

    Add the pocketKnife weapon

---
+++ app/src/main/java/com/example/gamescreen/ViewModel/Enemy/PocketKnife.java
@@ -0,0 +1,31 @@
+package com.example.gamescreen.ViewModel.Enemy;
+
+public class PocketKnife extends Weapon {
+    private int damage;
+    private int durability;
+    private boolean inUse;
+    private int critCount;
+    private int critical;
+    public PocketKnife(int damage, int durability, boolean inUse, int critical) {
+        this.damage = damage;
+        this.durability = durability;
+        this.inUse = inUse;
+        this.critical = critical;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
+    }
+}
